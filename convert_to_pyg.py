6f9828d69a7725b105c3d3904e083c4778cbb4b5
Eugen Miknevic <e.mikhnevych@gmail.com>
2026-01-21 08:13:44 +0100

    Translate comments to English

--- convert_to_pyg.py
+++ convert_to_pyg.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm import tqdm
 from torch_geometric.data import Data
 
-# Настройки
+# Settings
 RAW_DIR = "./data"
 PROC_DIR = "./data_pyg"
 os.makedirs(PROC_DIR, exist_ok=True)
@@ -18,7 +18,7 @@ for f_name in tqdm(all_files):
     npz_path = os.path.join(RAW_DIR, f_name)
     msh_path = os.path.join(RAW_DIR, base_name + '.msh')
 
-    # 1. Читаем меш (один раз на файл)
+    # 1. Read mesh (once per file)
     mesh = meshio.read(msh_path)
     pos = torch.from_numpy(mesh.points).float() / 1000.0
     cells = mesh.cells_dict.get('tetra', mesh.cells_dict.get('tetra4'))
@@ -30,13 +30,13 @@ for f_name in tqdm(all_files):
     markers = torch.from_numpy(mesh.point_data['markers']).long() if 'markers' in mesh.point_data else torch.zeros(pos.size(0), dtype=torch.long)
     markers_oh = torch.nn.functional.one_hot(markers, num_classes=3).float()
 
-    # 2. Читаем все снимки температур
+    # 2. Read all temperature snapshots
     data_npz = np.load(npz_path)
     temps = torch.from_numpy(data_npz['snapshots_nodal']).float() # (T, N)
     times = torch.from_numpy(data_npz['times']).float() # (T,)
 
-    # 3. Сохраняем компактный объект
-    # Мы сохраняем геометрию и ВСЕ временные шаги в один файл .pt
+    # 3. Save compact object
+    # We save geometry and ALL time steps into a single .pt file
     payload = {
         'pos': pos,
         'edge_index': edge_index,
